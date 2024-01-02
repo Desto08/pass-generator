0794e40cba709ce64e6841512a9f508d33c9a14d
Desto08 <David-espirito@hotmail.nl>
2024-01-02 11:08:00 +0100

    Extra functies

--- wachtwoordgenerator.go
+++ wachtwoordgenerator.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"math/rand"
 	"strconv"
+	"strings"
 	"time"
 
 	"fyne.io/fyne/v2/app"
@@ -17,38 +20,88 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// Database configuratiegegevens
 const (
 	dbname = "mijndb"
 	dbuser = "david"
 	dbpass = "geheim"
 )
 
-const letters string = "abcdfghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const numbers string = "0123456789"
-const symbols string = "!@#$%&*+_-="
+// letters bevat alle kleine en hoofdletters
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func MaakWachtwoord(length int, heeftnummers bool, heeftsymbolen bool) string {
-	chars := letters
-	if heeftnummers {
-		chars += numbers
-	}
-	if heeftsymbolen {
-		chars += symbols
+// numbers bevat alle cijfers van 0 tot 9
+const numbers = "0123456789"
+
+// symbols bevat een reeks speciale tekens
+const symbols = "!@#$%&*+_-="
+
+const (
+	// geeft het minimale aantal vereiste letters aan voor het wachtwoord
+	minLetters = 8
+	// geeft het minimale aantal vereiste nummers aan voor het wachtwoord
+	minNumbers = 2
+	// geeft het minimale aantal vereiste symbolen aan voor het wachtwoord
+	minSymbols = 1
+)
+
+// Genereert een wachtwoord op basis van opgegeven lengte en vereisten voor letters, cijfers en symbolen.
+// Als het opgegeven wachtwoord niet voldoet aan de vereisten, wordt een fout geretourneerd.
+func MaakWachtwoord(length, numLetters, numNumbers, numSymbols int) (string, error) {
+	requiredLetters := minLetters
+	requiredNumbers := minNumbers
+	requiredSymbols := minSymbols
+
+	if length != numLetters+numNumbers+numSymbols || numLetters < requiredLetters || numNumbers < requiredNumbers || numSymbols < requiredSymbols {
+		return "", fmt.Errorf("wachtwoord voldoet niet aan de vereisten")
 	}
 
-	return genereerWachtwoord(length, chars)
+	var chars strings.Builder
+	chars.WriteString(letters)
+	chars.WriteString(numbers)
+	chars.WriteString(symbols)
+
+	wachtwoord := generatePassword(length, numLetters, numNumbers, numSymbols, chars.String())
+
+	return wachtwoord, nil
 }
 
-func genereerWachtwoord(length int, chars string) string {
-	wachtwoord := ""
-	for i := 0; i < length; i++ {
-		wachtwoord += string([]rune(chars)[rand.Intn(len(chars))])
+// genereert een wachtwoord van de opgegeven lengte met het aangegeven aantal letters, cijfers en symbolen
+// gebaseerd op de beschikbare tekens in de 'chars'-string.
+// De resulterende wachtwoordstring wordt willekeurig geschud voordat deze wordt geretourneerd.
+func generatePassword(length, numLetters, numNumbers, numSymbols int, chars string) string {
+	var result strings.Builder
+
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	for i := 0; i < numLetters; i++ {
+		result.WriteByte(chars[rng.Intn(len(letters))])
 	}
-	return wachtwoord
+
+	for i := 0; i < numNumbers; i++ {
+		result.WriteByte(chars[len(letters)+rng.Intn(len(numbers))])
+	}
+
+	for i := 0; i < numSymbols; i++ {
+		result.WriteByte(chars[len(letters)+len(numbers)+rng.Intn(len(symbols))])
+	}
+
+	return shuffleString(result.String())
 }
 
-func hashWachtwoord(wachtwoord string) (string, error) {
-	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(wachtwoord), bcrypt.DefaultCost)
+// herschikt de tekens willekeurig in de opgegeven strings
+func shuffleString(s string) string {
+	r := []rune(s)
+	rand.Shuffle(len(r), func(i, j int) {
+		r[i], r[j] = r[j], r[i]
+	})
+	return string(r)
+}
+
+// genereert een unieke hash op basis van de combinatie van gebruikersnaam en wachtwoord.
+func hashUnique(username, password string) (string, error) {
+	combined := username + password
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(combined), bcrypt.DefaultCost)
 	if err != nil {
 		return "", err
 	}
@@ -56,70 +109,100 @@ func hashWachtwoord(wachtwoord string) (string, error) {
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Wachtwoordgenerator")
 
 	lengthEntry := widget.NewEntry()
 	lengthEntry.SetPlaceHolder("Lengte van het wachtwoord")
-	numbersCheck := widget.NewCheck("Nummers toestaan", func(checked bool) {})
-	symbolsCheck := widget.NewCheck("Symbolen toestaan", func(checked bool) {})
+
+	lettersEntry := widget.NewEntry()
+	lettersEntry.SetPlaceHolder("Aantal letters")
+
+	numbersEntry := widget.NewEntry()
+	numbersEntry.SetPlaceHolder("Aantal cijfers")
+
+	symbolsEntry := widget.NewEntry()
+	symbolsEntry.SetPlaceHolder("Aantal symbolen")
 
 	wachtwoordLabel := widget.NewLabel("")
-
-	generateButton := widget.NewButtonWithIcon("Genereer wachtwoord", theme.ContentAddIcon(), func() {
-		length, err := strconv.Atoi(lengthEntry.Text)
-		if err != nil {
-			dialog.ShowError(err, myWindow)
-			return
-		}
-
-		db, err := sql.Open("postgres", "dbname="+dbname+" user="+dbuser+" password="+dbpass+" sslmode=disable")
-		if err != nil {
-			dialog.ShowError(err, myWindow)
-			return
-		}
-		defer db.Close()
-
-		var wachtwoord string
-		loopActief := true
-
-		for loopActief {
-			wachtwoord = MaakWachtwoord(length, numbersCheck.Checked, symbolsCheck.Checked)
-			var count int
-			err := db.QueryRow("SELECT COUNT(*) FROM gebruikers WHERE wachtwoord = $1", wachtwoord).Scan(&count)
-			if err != nil {
-				dialog.ShowError(err, myWindow)
-				return
-			}
-			if count == 0 {
-				loopActief = false
-			}
-		}
-
-		hashedWachtwoord, err := hashWachtwoord(wachtwoord)
-		if err != nil {
-			dialog.ShowError(err, myWindow)
-			return
-		}
-		_, err = db.Exec("INSERT INTO gebruikers (gebruikersnaam, wachtwoord) VALUES ($1, $2)", "David", hashedWachtwoord)
-		if err != nil {
-			dialog.ShowError(err, myWindow)
-			return
-		}
-		wachtwoordLabel.SetText("Gegenereerd wachtwoord: " + wachtwoord)
-		dialog.ShowInformation("Wachtwoord opgeslagen", "Wachtwoord is gegenereerd en opgeslagen in de database", myWindow)
-	})
 
 	content := container.NewVBox(
 		layout.NewSpacer(),
 		widget.NewForm(
 			widget.NewFormItem("Lengte:", lengthEntry),
-			widget.NewFormItem("", numbersCheck),
-			widget.NewFormItem("", symbolsCheck),
+			widget.NewFormItem("Aantal letters:", lettersEntry),
+			widget.NewFormItem("Aantal cijfers:", numbersEntry),
+			widget.NewFormItem("Aantal symbolen:", symbolsEntry),
 		),
-		generateButton,
+		widget.NewButtonWithIcon("Genereer wachtwoord", theme.ContentAddIcon(), func() {
+			length, err := strconv.Atoi(lengthEntry.Text)
+			if err != nil {
+				dialog.ShowError(err, myWindow)
+				return
+			}
+
+			numLetters, err := strconv.Atoi(lettersEntry.Text)
+			if err != nil {
+				dialog.ShowError(err, myWindow)
+				return
+			}
+
+			numNumbers, err := strconv.Atoi(numbersEntry.Text)
+			if err != nil {
+				dialog.ShowError(err, myWindow)
+				return
+			}
+
+			numSymbols, err := strconv.Atoi(symbolsEntry.Text)
+			if err != nil {
+				dialog.ShowError(err, myWindow)
+				return
+			}
+
+			if numLetters+numNumbers+numSymbols != length {
+				dialog.ShowError(errors.New("de som van opgegeven aantallen komt niet overeen met de totale lengte"), myWindow)
+				return
+			}
+
+			wachtwoord, err := MaakWachtwoord(length, numLetters, numNumbers, numSymbols)
+			if err != nil {
+				dialog.ShowError(errors.New("wachtwoord voldoet niet aan de minimale vereisten"), myWindow)
+				return
+			}
+
+			db, err := sql.Open("postgres", "dbname="+dbname+" user="+dbuser+" password="+dbpass+" sslmode=disable")
+			if err != nil {
+				dialog.ShowError(err, myWindow)
+				return
+			}
+			defer db.Close()
+
+			// Controleerd of de wachtwoord al bestaat voor de specifiek genoemde gebruikersnaam
+			var count int
+			err = db.QueryRow("SELECT COUNT(*) FROM gebruikers WHERE gebruikersnaam = $1 AND wachtwoord = $2", "David2", wachtwoord).Scan(&count)
+			if err != nil {
+				dialog.ShowError(err, myWindow)
+				return
+			}
+			if count != 0 {
+				dialog.ShowInformation("Wachtwoord bestaat al", "Dit wachtwoord bestaat al in de database voor deze gebruiker", myWindow)
+				return
+			}
+
+			hashedUnique, err := hashUnique("David2", wachtwoord)
+			if err != nil {
+				dialog.ShowError(err, myWindow)
+				return
+			}
+
+			_, err = db.Exec("INSERT INTO gebruikers (gebruikersnaam, wachtwoord) VALUES ($1, $2)", "David2", hashedUnique)
+			if err != nil {
+				dialog.ShowError(err, myWindow)
+				return
+			}
+			wachtwoordLabel.SetText("Gegenereerd wachtwoord: " + wachtwoord)
+			dialog.ShowInformation("Wachtwoord opgeslagen", "Wachtwoord is gegenereerd en opgeslagen in de database", myWindow)
+		}),
 		wachtwoordLabel,
 		layout.NewSpacer(),
 	)
